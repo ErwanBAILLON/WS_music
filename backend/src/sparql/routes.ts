7188794f66d1a1849712c622c261d5897b151bdc
Michael Schubert <ms@asello.eu>
2024-01-06 15:23:22 +0100

    Changed the random selection of songs

--- backend/src/sparql/routes.ts
+++ backend/src/sparql/routes.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/sparql', async (req: Request, res: Response) => {
   const endpoint = 'http://dbpedia.org/sparql';
-  const random = getRandomArbitrary(1, 100);
+  const random = randomIntFromInterval(1, 100);
 
   let result = new QuizResult();
 
@@ -36,7 +36,7 @@ router.get('/sparql', async (req: Request, res: Response) => {
   })
     .then((response) => {
       const data = response.data;
-      console.log(data.results.bindings);
+      // console.log(data.results.bindings);
 
       result.artist = response.data.results.bindings[0].artistName.value;
       result.rightSong = response.data.results.bindings[0].songName.value;
@@ -59,8 +59,7 @@ router.get('/sparql', async (req: Request, res: Response) => {
           ?artist foaf:name ?artistName .
           FILTER (?artistName != '${response.data.results.bindings[0].artistName}')
         }
-        ORDER BY ?s OFFSET ${random}
-        LIMIT 3
+        LIMIT 250
         `;
 
 
@@ -72,14 +71,26 @@ router.get('/sparql', async (req: Request, res: Response) => {
       })
         .then((response2) => {
           const data = response2.data;
-          console.log(data.results.bindings);
+          // console.log(data.results.bindings);
 
-          if(!result.wrongSongs)
+          if (!result.wrongSongs)
             result.wrongSongs = [];
 
-          response2.data.results.bindings.forEach((current: any) => {
-            result.wrongSongs?.push(current.songName.value);
-          })
+          let random2 = randomIntFromInterval(1, 250);
+          let random3 = randomIntFromInterval(1, 250);
+          let random4 = randomIntFromInterval(1, 250);
+
+          while(random3 == random2 || random3 == random4){
+            random3 = randomIntFromInterval(1, 250);
+          }
+
+          while(random4 == random2){
+            random4 = randomIntFromInterval(1, 250);
+          }
+
+          result.wrongSongs?.push(response2.data.results.bindings[random2].songName.value);
+          result.wrongSongs?.push(response2.data.results.bindings[random3].songName.value);
+          result.wrongSongs?.push(response2.data.results.bindings[random4].songName.value);
 
           return result;
         })
@@ -91,11 +102,11 @@ router.get('/sparql', async (req: Request, res: Response) => {
 });
 
 
-function getRandomArbitrary(min: number, max: number) {
-  return Math.random() * (max - min) + min;
+function randomIntFromInterval(min: number, max: number) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-class QuizResult{
+class QuizResult {
   artist?: string;
   rightSong?: string;
   wrongSongs?: string[];
